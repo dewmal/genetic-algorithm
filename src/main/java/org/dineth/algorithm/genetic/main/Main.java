6bd52ba336991186e27be3282e054e39a77a2120
DewmalPC <dewmalnilanka@gmail.com>
2013-07-13 22:07:34 +0530

    initialize project

---
+++ src/main/java/org/dineth/algorithm/genetic/main/Main.java
@@ -0,0 +1,11 @@
+package org.dineth.algorithm.genetic.main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dewmalpc
+ * Date: 7/13/13
+ * Time: 9:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
